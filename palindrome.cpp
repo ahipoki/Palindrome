d16293692211d919fe9498c1b925f0d5aac22465
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-09-25 13:40:12 -0700

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -11,19 +11,25 @@ int main()
   char output[80];
   //Char array for output 80 chars
   cout << "Enter a phrase to test if it is a palindrome." << endl;
+  //Tell the user to enter a phrase
   cin.getline(input, 80);
+  //Get the user's input and store it in input array
   cin.get();
-  //Read in user's input which stores as input char array
+  //Prevent garbage from going in the array
 
   int i = strlen(input) - 1, j = 0;
-  while (i >= 0) {
+  while (i >= 0) {//While loop for reversing the phrase
     output[j] = input[i];
+    //Set the last one in the output array to the first one in the input array
     i--;
+    //Move 1 forwards in the input array
     j++;
+    //Move 1 back in the output array
   }
 
-  while (k <= strlen(input)) {
+  while (k <= strlen(input)) {//While you you're still going through the input array
     input[k] = tolower(input[k]);
+    //Set any characters in the array as a lowercase
   }
   
   while (l <= strlen(input)) {
@@ -32,6 +38,7 @@ int main()
       l++;
     }
   }
+  
   if (ispunct(input[l] == true)) {
     //input[l] = input[m];
     //output[m] = output[l];
@@ -39,11 +46,13 @@ int main()
   }
   cout << output << endl;
   
-  if (strcmp(input, output) == 0) {
+  if (strcmp(input, output) == 0) {//If the two arrays are equal
     cout << "Palindrome" << endl;
+    //Tell the user that it's a palindrome
   }
-  else if (strcmp(input, output) != 0) {
+  else if (strcmp(input, output) != 0) {//If the two arrays are not equal
     cout << "Not a Palindrome" << endl;
+    //Tell the user that it's not a palindrome
   }
 
   //cout << input << endl;
